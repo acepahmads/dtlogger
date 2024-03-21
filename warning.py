ee9e414370b1b6fc58d9ab811c0e8f94bf06cd1b
acepahmads <157031144+acepahmads@users.noreply.github.com>
2024-03-21 10:24:29 +0700

    Add files via upload

--- warning.py
+++ warning.py
@@ -3,9 +3,9 @@ import time
 import requests
 import datetime
 import mysql.connector
+import json
 
-def send_telegram_message(bot_token, chat_id, message):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+def send_telegram_message(url, chat_id, message):
     params = {
         "chat_id": chat_id,
         "text": message
@@ -14,10 +14,31 @@ def send_telegram_message(bot_token, chat_id, message):
     return response.json()
 
 def send_warning_tele(message):
-    bot_token = "7013180303:AAEN1duZ6W4L__pBX6l32OnieQ-BZy6rSJ8"
-    chat_id = "389139071"
-    response = send_telegram_message(bot_token, chat_id, message)
-    print(response)
+    try:
+        url = ""
+        chat_id = ""
+        #{"url": "https://api.telegram.org/bot7013180303:AAEN1duZ6W4L__pBX6l32OnieQ-BZy6rSJ8/sendMessage",
+        # "chat_id": "389139071"}
+        with open('/home/cbi/dtlogger/config/telegram.txt', 'r') as file:
+        #with open('telegram.txt', 'r') as file:
+            # Read the entire contents of the file
+            content = file.read()
+            print(content)
+            json_data = json.loads(content)
+            print(json_data["url"], json_data["chat_id"])
+            url = json_data["url"]
+            chat_id = json_data["chat_id"]
+
+        if ((url == "") or (chat_id == "")):
+            print("please check telegram.txt config")
+        else:
+            response = send_telegram_message(url, chat_id, message)
+            print(response)
+    except FileNotFoundError:
+        print("File 'telegram.txt' not found")
+
+    except Exception as e:
+        print("An error occurred:", e)
 
 def getdata(fromd, to, value):
     try:
@@ -45,7 +66,7 @@ def getdata(fromd, to, value):
         rows = cursor.rowcount
         print("data transmission completeness checking : ", rows)
         if (rows < times):
-            message = f"{value} [{to}] : incomplete send message(s) until now"
+            message = f"{value} [{to}] : Data sent = {rows}, it should be {times}"
             send_warning_tele(message)
 
         #message waiting checking
@@ -58,7 +79,7 @@ def getdata(fromd, to, value):
         rows = cursor.rowcount
         print("message waiting checking : ", rows)
         if (rows > 0):
-            message = f"{value} [{to}] : {rows} message(s) waiting/failed to KLHK today"
+            message = f"{value} [{to}] : {rows} sending message(s) is 'waiting/failed' to KLHK"
             send_warning_tele(message)
 
         #generate refrence checking
@@ -71,7 +92,7 @@ def getdata(fromd, to, value):
         rows = cursor.rowcount
         print("generate refrence checking : ", rows)
         if (rows == 0):
-            message = f"{value} [{to}] : The system did not generate reference today"
+            message = f"{value} [{to}] : Fatal error, the system did not generate reference today, please check Immediately"
             send_warning_tele(message)
 
         if connection.is_connected():
@@ -84,6 +105,24 @@ def getdata(fromd, to, value):
 
 
 def main(value):
+    """
+    try:
+        value1 = 36.1
+        print("value : ", value1)
+        formula = "((3.76215537+0.52968523*value)/(1+0.06531247*value-0.00021621*(value**2))*0.0042)*((64/24.4)*1000)*0.73"
+        print("formula : ", formula)
+
+        value = eval(formula)
+        print("value : ", value1)
+    except Exception as error:
+        print("An error occurred:", error)
+
+    print("test12")
+    """
+    now = datetime.datetime.now()
+    message = f"{value} [{now}] : The system is started"
+    send_warning_tele(message)
+
     while (True):
         now = datetime.datetime.now()
         print(now)
@@ -103,7 +142,6 @@ def main(value):
 
             getdata(fromd, to, value)
         time.sleep(1)
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         main(sys.argv[1])
