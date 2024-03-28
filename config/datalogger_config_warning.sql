11a19be36fa17a78eaa4b6a5bd12126f73e2e4d1
acepahmads <157031144+acepahmads@users.noreply.github.com>
2024-03-28 16:10:12 +0700

    Add files via upload

--- config/datalogger_config_warning.sql
+++ config/datalogger_config_warning.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
 -- Host: localhost    Database: wqms_onlimo
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+-- Server version	8.0.36-0ubuntu0.22.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,15 +23,17 @@ DROP TABLE IF EXISTS `datalogger_config_warning`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `datalogger_config_warning` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `method` text NOT NULL,
-  `h-hour` varchar(45) NOT NULL,
-  `expression` text NOT NULL,
-  `key` varchar(45) NOT NULL,
-  `message` varchar(100) NOT NULL,
-  `description` text DEFAULT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `method` text COLLATE utf8mb4_general_ci NOT NULL,
+  `h-hour` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `expression` text COLLATE utf8mb4_general_ci NOT NULL,
+  `key` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `message` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `min` text COLLATE utf8mb4_general_ci,
+  `max` text COLLATE utf8mb4_general_ci,
+  `description` text COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +42,7 @@ CREATE TABLE `datalogger_config_warning` (
 
 LOCK TABLES `datalogger_config_warning` WRITE;
 /*!40000 ALTER TABLE `datalogger_config_warning` DISABLE KEYS */;
-INSERT INTO `datalogger_config_warning` VALUES (1,'send_klhk','3','uploaded_klhk == 3','check_transmission','Data sent KLHK = {data_sent}, it should be {count}','Cek jumlah pengiriman ke KLHK hari ini'),(2,'send_klhk','3','uploaded_klhk != 3','check_waiting','{count} message(s) \'waiting/failed\' send to KLHK','Cek jumlah pengiriman ke KLHK yang tidak berhasil hari ini'),(3,'send_klhk','3','','check_gen_refrence','Fatal error, the system did not generate reference today, please check Immediately','Cek pembuatan generate refrence hari ini'),(4,'read_value','1','value < 7 and value >300','cod','Anomali COD ','Cek value COD'),(5,'read_value','1','f\"value != {cod*0.5} or value != {cod*0.25}\"','bod','Anomali BOD','Cek value BOD dari COD'),(6,'read_value','1','f\"value < {bod} and value > {cod}\"','toc','Anomali TOC','Cek value TOC dari COD dan BOD'),(7,'read_value','1','value < 10 and value > 60','tss','Anomali TSS','Cek value TSS'),(8,'read_value','1','f\"value < {tss}\"','turbidity','Anomali Turbidity','Cek value Turbidity'),(9,'read_value','1','value < 6 and value > 9','ph','Anomali PH','Cek value PH'),(10,'read_value','1','value < 1 and value > 1','amonia','Anomali amonia','Cek value amonia');
+INSERT INTO `datalogger_config_warning` VALUES (1,'send_klhk','3','uploaded_klhk = 3','check_transmission','f\"Data sent KLHK = {data_sent}, it should be {count}\"',NULL,NULL,'Cek jumlah pengiriman ke KLHK hari ini'),(2,'send_klhk','3','uploaded_klhk != 3','check_waiting','f\"{count} message(s) \'waiting/failed\' send to KLHK\"',NULL,NULL,'Cek jumlah pengiriman ke KLHK yang tidak berhasil hari ini'),(3,'send_klhk','3','','check_gen_refrence','f\"Fatal error, the system did not generate reference today, please check Immediately\"',NULL,NULL,'Cek pembuatan generate refrence hari ini'),(4,'read_value','1','value < 7 and value >300','cod','Anomali COD ',NULL,NULL,'Cek value COD'),(6,'read_value','1','f\"value < {bod} and value > {cod}\"','toc','Anomali TOC',NULL,NULL,'Cek value TOC dari COD dan BOD'),(7,'read_value','1','value < 10 and value > 60','tss','Anomali TSS',NULL,NULL,'Cek value TSS'),(8,'read_value','1','f\"value < {tss}\"','turbidity','Anomali Turbidity',NULL,NULL,'Cek value Turbidity'),(9,'read_value','1','value < 6 and value > 9','ph','Anomali PH',NULL,NULL,'Cek value PH'),(10,'read_value','1','value < 0.02 and value > 2','nh3','Anomali amonia',NULL,NULL,'Cek value amonia'),(11,'read_value','1','f\"value < {cod}*0.5 or value < {cod}*0.25\"','bod','Anomali BOD',NULL,NULL,'Cek value BOD dari COD'),(12,'read_value','1','f\"value > {cod}*0.5 or value > {cod}*0.25\"','bod','Anomali BOD',NULL,NULL,'Cek value BOD dari COD'),(13,'read_value','1','value < 0.02 and value > 2','no3','Anomali nitrat',NULL,NULL,'Cek value amonia');
 /*!40000 ALTER TABLE `datalogger_config_warning` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-23  4:30:22
+-- Dump completed on 2024-03-28 16:05:49
